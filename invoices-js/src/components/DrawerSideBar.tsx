b9796a1583c7018e20f005540708149d09cf1b1c
CAWilson94 <charlotteawilson.94@gmail.com>
2022-05-25 12:05:53 +0100

    drawer open set to false for now

--- invoices-js/src/components/DrawerSideBar.tsx
+++ invoices-js/src/components/DrawerSideBar.tsx
@@ -4,7 +4,7 @@ import Drawer from '@mui/material/Drawer';
 const drawerWidth = 240;
 
 export default function DrawerSideBar() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
