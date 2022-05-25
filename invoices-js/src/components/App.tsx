dc55827bf83c9d912f1e8eb4230025b98e14e139
CAWilson94 <charlotteawilson.94@gmail.com>
2022-05-25 12:05:09 +0100

    basic drawer showing open

--- invoices-js/src/components/App.tsx
+++ invoices-js/src/components/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import Header from './Header';
+import DrawerSideBar from './DrawerSideBar';
 
 function App() {
   return (
-    <Header />
+    <><Header /><DrawerSideBar /></>
   );
 }
 
