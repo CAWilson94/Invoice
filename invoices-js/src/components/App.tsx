118aaa6fb752e37f19b3f4843c3d9584d7889905
CAWilson94 <charlotteawilson.94@gmail.com>
2022-05-27 18:21:38 +0100

    basic redux before the great slice

--- invoices-js/src/components/App.tsx
+++ invoices-js/src/components/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import Header from './Header';
-import DrawerSideBar from './DrawerSideBar';
+import React, { Fragment } from "react";
+import logo from "../logo.svg";
+import "../App.css";
+import Header from "./Header";
+import DrawerSideBar from "./DrawerSideBar";
 
 function App() {
   return (
-    <><Header /><DrawerSideBar /></>
+    <Fragment>
+      <Header />
+      <DrawerSideBar />
+    </Fragment>
   );
 }
 
--- invoices-js/src/index.tsx
+++ invoices-js/src/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store";
+import { Provider } from "react-redux";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
